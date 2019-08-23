f7193aa2e8a912354c53c4b14521bf57aff56ae9
Jimmique <jimmique88@gmail.com>
2019-08-22 23:43:45 -0700

    added more component files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TabNav from "./components/TabNav.js.js";
+import Header from "./components/Header.js.js";
+import AppRouter from "./components/AppRouter.js.js";
+
+export default function App() {
+  return (
+    <main>
+      <Header />
+      <TabNav />
+      {/*<AppRouter /> */}
+    </main>
+  );
+}
+
+export default App;
