65bebd7309cf6437f85677a58f19684b03e801aa
Jimmique <jimmique88@gmail.com>
2019-08-22 23:10:01 -0700

    added react dependencies, semantic dependencies, and updatted the Header and TabNav files.

---
+++ components/TabNav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Tab, Menu, Icon } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
+
+export default function TabNav() {	
+
+ const panes = [
+  { menuItem: {key: 'students', icon: '', content: 'Students List' },
+    render: () => <Tab.Pane>Students</Tab.Pane>,
+  },
+  { menuItem: {key: 'messages', icon: '', content: 'Messaging' },
+    render: () => <Tab.Pane>Messaging</Tab.Pane>,
+  },
+
+];
+
+ export default function TabNav() {
+  return (
+    <Tab panes={panes} />
+  )
+};	};
