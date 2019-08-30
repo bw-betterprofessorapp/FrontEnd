18cac024026611da8b05a7ece027d7b47e641d1a
Jordin Kolman <git@github.com>
2019-08-30 11:14:21 -0500

    Cleaned code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './App.css';
-import { Container, Header } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom'
+import { Container } from 'semantic-ui-react';
 import AppRouter from './components/AppRouter'
-import TabNav from "./components/TabNav.js";
-import Header from "./components/Header.js";
 
 
 function App() {
--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,7 +1,5 @@
 import React from "react";
-import StudentList from "./StudentList.js";
-import Messanger from "./Messanger.js";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import FormikLogInForm from "./LogIn";
 import FormikSignUpForm from "./SignUp";
@@ -9,32 +7,12 @@ import LandingPage from "./LandingPage";
 
 export default function AppRouter() {
 
-  const PrivateRouter = ({ component: Component, ...rest }) => (
-    <Route
-      {...rest}
-      render={props =>
-        localStorage.getItem('token') ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to='/login' />
-        )
-      }
-    />
-  )
-
-       <Route 
-        path="/messages" 
-        render={props => <Messanger {...props} />}
-      />
-
-
-    </div>
   return (
     <Container className="app-router">
       <Switch>
         <Route path="/login" component={FormikLogInForm} />
         <Route path="/signup" component={FormikSignUpForm} />
-        <PrivateRouter path="/landingpage" component={LandingPage} />
+        <Route path="/landingpage" component={LandingPage} />
       </Switch>
     </Container>
   );
