792826d6880aa0ea7fc41b2143af26cd71509d1f
Jordin Kolman <git@github.com>
2019-08-30 11:18:04 -0500

    fixed routing

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -4,38 +4,23 @@ import { Container } from "semantic-ui-react";
 import FormikLogInForm from "./LogIn";
 import FormikSignUpForm from "./SignUp";
 import LandingPage from "./LandingPage";
+import Messanger from './Messanger'
 
 export default function AppRouter() {
 
-<<<<<<< HEAD
-=======
-  const PrivateRouter = ({ component: Component, ...rest }) => (
-    <Route
-      {...rest}
-      render={props =>
-        localStorage.getItem('token') ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to='/login' />
-        )
-      }
-    />
-  );
-
-       <Route 
-        path="/messages" 
-        render={props => <Messanger {...props} />}
-      />
+  
 
 
-
->>>>>>> origin/master
   return (
     <Container className="app-router">
       <Switch>
         <Route path="/login" component={FormikLogInForm} />
         <Route path="/signup" component={FormikSignUpForm} />
         <Route path="/landingpage" component={LandingPage} />
+        <Route 
+        path="/messages" 
+        render={props => <Messanger {...props} />}
+      />
       </Switch>
     </Container>
   );
